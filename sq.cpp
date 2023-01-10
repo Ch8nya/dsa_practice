bafb1784f2a20f2e7aaf6450405ff30d08f6d8ae
Ch8nya <90951141+Ch8nya@users.noreply.github.com>
2023-01-10 15:07:54 +0530

    Update sq.cpp

--- sq.cpp
+++ sq.cpp
@@ -1,126 +1,215 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
-class stack{
-  int top;
-  int arr[20];
-  public:
-  stack(){
-    top = -1;
-  }
-  void push(int data);
-  int pop();
-  bool isempty();
-  bool isFull();
-  void display();
+class SLL
+{
+public:
+struct Node
+{
+int data;
+Node * next;
+} ;
+Node * Top;
+Node *rear;
+Node *front;
+SLL()
+{
+Top=nullptr;
+rear=nullptr;
+front = nullptr;
+}
+void Push();
+void Pop();
+void display_stack();
+int stack_empty();
+void enqueue();
+void dequeue();
+void display_queue();
+int queue_empty();
 };
-
-void stack::push(int data){
-  if(isFull()){
-    cout << "Stack OverFlow\n";
-  }
-  else{
-    top++;
-    arr[top] = data;
-  }
+int main()
+{
+SLL s1,q1;
+int choice,stchoice,qchoice;
+cout<<"***********************WELCOME************************"<<endl;
+a:
+do
+{
+cout<<"\nMENU\n1.STACK operations\n2.QUEUE operations\n3.EXIT"<<endl;
+cout<<"Enter your choice: ";
+cin>>choice;
+switch(choice)
+{
+case 1:
+b:
+do
+{
+cout<<"\n1.Insert in stack\n2.Delete from stack\n3.Display stack content\n4.EXIT"<<endl;
+cout<<"Enter your choice: ";
+cin>>stchoice;
+switch(stchoice)
+{
+case 1:
+s1.Push();
+break;
+case 2:
+s1.Pop();
+break;
+case 3:
+s1.display_stack();
+break;
+case 4:
+goto a;
+break;
+default:
+cout<<"Enter valid choice!!!"<<endl;
+goto b;
 }
-
-int stack::pop(){
-  int x = 0;
-  if(isempty()){
-    cout << "Stack UnderFlow\n";
-    
-  }else{
-    x = arr[top];
-    top--;
-  }
-  return x;
+}while(stchoice<5);
+break;
+case 2:
+c:
+do
+{
+cout<<"\n1.Insert in Queue\n2.Delete from Queue\n3.Display Queue content\n4.EXIT"<<endl;
+cout<<"Enter your choice: ";
+cin>>qchoice;
+switch(qchoice)
+{
+case 1:
+q1.enqueue();
+break;
+case 2:
+q1.dequeue();
+break;
+case 3:
+q1.display_queue();
+break;
+case 4:
+goto a;
+break;
+default:
+cout<<"Enter valid choice!!!"<<endl;
+goto c;
 }
-
-bool stack::isFull(){
-  if(top==19){
-    return 1;
-  }
-  return 0;
+}while(stchoice<5);
+break;
+case 3:
+cout<<"THANKYOU !!!"<<endl;
+return 0;
 }
-
-bool stack::isempty(){
-  if(top==-1){
-    return 1;
-  }
-  return 0;
+}while(choice<4);
+return 0;
 }
-
-void stack::display(){
-  for(int i=0;i<=top;i++){
-    cout << arr[i] << " ";
-  }
-  cout << endl;
+int SLL :: stack_empty()
+{
+int flag=0;
+if(Top==nullptr)
+{
+flag=1;
 }
-
-class queue{
-  int front, rear;
-  int arr[20];
-  public:
-  queue(){
-    front = rear = -1;
-  }
-  void enqueue();
-  int dequeue();
-  bool isEmpty();
-  bool isFull();
-};
-
-void queue::enqueue(){
-  if
+return flag;
 }
-
-int queue::dequeue(){
-  
+void SLL :: Push()
+{
+Node *nn = new Node;
+cout<<"Enter the data: ";
+cin>>nn->data;
+if(Top==nullptr)
+{
+Top=nn;
+cout<<"\nPushed in stack successfully.\n";
 }
-
-bool queue::isFull(){
-  if(rear == 19){
-    return 1;
-  }
-  return 0;
+else
+{
+nn->next=nullptr;
+nn->next=Top;
+Top=nn;
+cout<<"\nPushed in stack successfully.\n";
 }
-
-bool queue::isEmpty(){
-  if(front > rear){
-    return 1;
-  }
-  return 0;
 }
-
-int main() {
-  stack s;
-  s.push(1);
-  s.push(2);
-  s.push(3);
-  s.push(4);
-  s.push(5);
-  s.push(6);
-  s.push(7);
-  s.push(8);
-  s.push(9);
-  s.push(120);
-  s.push(99);
-  s.push(12);
-  s.display();
-  cout << s.pop() << endl;
-  cout << s.pop() << endl;
-  cout << s.pop() << endl;
-  cout << s.pop() << endl;
-  cout << s.pop() << endl;
-  cout << s.pop() << endl;
-  cout << s.pop() << endl;
-  cout << s.pop() << endl;
-  cout << s.pop() << endl;
-  cout << s.pop() << endl;
-  cout << s.pop() << endl;
-  cout << s.pop() << endl;
-  cout << s.pop() << endl;
-  
-  cout << "Hello World!\n";
+void SLL :: Pop()
+{
+int flag;
+flag=stack_empty();
+if(flag==1)
+{
+ cout<<"\nThe stack is empty . No elements can be deleted.";
+}
+else
+{
+Node * temp;
+temp=Top;
+Top=Top-> next;
+delete temp;
+cout<<"\nDeleted the Top element successfully.";
+}
+}
+void SLL :: display_stack()
+{
+Node *cn;
+cn=Top;
+cout<<"-----------------------------------------------------"<<endl;
+while(cn!=nullptr)
+{
+cout<<cn->data<<"\n";
+cn=cn->next;
+}
+cout<<"-----------------------------------------------------"<<endl;
+}
+int SLL :: queue_empty()
+{
+int flag=0;
+if(rear==nullptr && front == nullptr)
+{
+flag=1;
+}
+return flag;
+}
+void SLL :: enqueue()
+{
+ Node *cn,*nn;
+nn = new Node;
+cout<<"\nEnter the data: ";
+cin>>nn->data;
+nn->next=nullptr;
+ cn=front;
+if(front==nullptr)
+{
+front =nn;
+rear=nn;
+}
+else
+{
+rear->next=nn;
+rear =nn;
+}
+  cout<<"\nStored in the Queue Successfully. ";
+}
+void SLL :: dequeue()
+{
+int flag;
+flag=queue_empty();
+if(flag==1)
+{
+cout<<"\nThe Queue is empty.Nothing to delete."<<endl;
+}
+else{
+Node * temp;
+temp=front;
+front=front->next;
+delete temp;
+cout<<"\nDeleted Successfully.";
+}
+}
+void SLL :: display_queue()
+{
+Node * cn;
+cn=front;
+cout<<"-----------------------------------------------------"<<endl;
+while(cn!=nullptr)
+{
+cout<<cn->data<<endl;
+cn=cn->next;
+}
+cout<<"-----------------------------------------------------"<<endl;
 }
